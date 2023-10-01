aabe455f52bde1f44d8c4b167b01eccbc8ecce69
lyx <1197744123@sjtu.edu.cn>
2023-10-01 23:41:44 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
-git clone https://ipads.se.sjtu.edu.cn:1312/liuyuxuan/qemu-ae
-git clone https://ipads.se.sjtu.edu.cn:1312/liuyuxuan/linux-host-ae
+# only on arm-o AE machine
+mv ~/rootfs.qcow2 image/
+mv ~/vm-kernel-xtq.img image/
+
+qemu-img create -f raw image/vm-kernel-xtq-1.img 4G
+qemu-img create -f raw image/vm-kernel-xtq-2.img 4G
+qemu-img create -f raw image/vm-kernel-xtq-3.img 4G
+qemu-img create -f raw image/vm-kernel-xtq-4.img 4G
+mkfs.ext4 image/vm-kernel-xtq-1.img
+mkfs.ext4 image/vm-kernel-xtq-2.img
+mkfs.ext4 image/vm-kernel-xtq-3.img
+mkfs.ext4 image/vm-kernel-xtq-4.img
+
+git clone https://github.com/CPS-ae/linux-host-ae.git
+git clone https://github.com/CPS-ae/qemu-ae.git
 sudo mount image/vm-kernel-xtq.img mnt
 mkdir -p mnt
 cd mnt
-git clone https://ipads.se.sjtu.edu.cn:1312/liuyuxuan/linux-guest-ae
+git clone https://github.com/CPS-ae/linux-guest-ae.git
 cd -
 sudo umount mnt
 
